cfb9a2d1c9656d37623d1e445b80c0824b423d4e
KienPD28 <kienpdhe170155@fpt.edu.vn>
2024-03-07 21:30:07 +0700

    Connect With Database
    connect With DB [My_FAP_Project]

--- PRJ301_Assignment_FAP/src/java/DAO/DBContext.java
+++ PRJ301_Assignment_FAP/src/java/DAO/DBContext.java
@@ -4,11 +4,34 @@
  */
 package DAO;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author DELL
  */
 public class DBContext {
+    protected Connection connection;
+    public DBContext()
+    {
+        try{
+            String user="sa";
+            String pass="123";
+            String url="jdbc:sqlserver://localhost:1433;databaseName=MY_FAP_PROJECT";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection = DriverManager.getConnection(url, user, pass);
+        }
+        catch(Exception ex)
+        {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }    
     
-    
+    public static void main(String[] args) {
+        DBContext db = new DBContext();
+        System.out.println(db.connection);
+    }
 }
