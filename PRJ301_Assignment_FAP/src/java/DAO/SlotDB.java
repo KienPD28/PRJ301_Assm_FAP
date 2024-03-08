7efabae6aa38b93708eb71ef6f927e8af95ff75a
KienPD28 <kienpdhe170155@fpt.edu.vn>
2024-03-08 13:02:13 +0700

    Update SlotDB.java
    Create functions to retrieve slot information and data

--- PRJ301_Assignment_FAP/src/java/DAO/SlotDB.java
+++ PRJ301_Assignment_FAP/src/java/DAO/SlotDB.java
@@ -4,10 +4,66 @@
  */
 package DAO;
 
+import Entity.Slot;
+import java.util.ArrayList;
+import java.util.List;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author DELL
  */
-public class SlotDB {
+public class SlotDB extends DBContext {
+
+    public List<Slot> getAllSlot() {
+        List<Slot> list = new ArrayList<>();
+        try {
+            String sql = "SELECT [Slid]\n"
+                    + "      ,[Slname]\n"
+                    + "  FROM [MY_FAP_PROJECT].[dbo].[Slot]";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ResultSet rs  = ps.executeQuery();
+            while(rs.next()){
+                Slot s = new Slot();
+                s.setSlid(rs.getInt("Slid"));
+                s.setSlname(rs.getString("Slname"));
+                list.add(s);
+            }
+            
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(SlotDB.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return list;
+    }
     
+    public Slot getSlotBySlotId(int Slid) {
+        List<Slot> list = new ArrayList();
+        try {
+            String sql = "SELECT [Slid]\n"
+                    + "      ,[Slname]\n"
+                    + "  FROM [MY_FAP_PROJECT].[dbo].[Slot]\n"
+                    + "  WHERE [Slid] = ?";
+            PreparedStatement stm = connection.prepareStatement(sql);
+            stm.setInt(1, Slid);
+            ResultSet rs = stm.executeQuery();
+            if (rs.next()) {
+                Slot s = new Slot(rs.getInt("Slid"), rs.getString("Slname"));
+                return s;
+            }
+        } catch (SQLException ex) {
+
+        }
+        return null;
+
+    }
+    
+     public static void main(String[] args) {
+        SlotDB db = new SlotDB();
+        int s = db.getAllSlot().size();
+        System.out.println(s);
+    }
+
 }
